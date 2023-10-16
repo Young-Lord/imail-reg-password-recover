3cc16308034f58570a78f61eb900e6c42d2111e9
LY <51789698+Young-Lord@users.noreply.github.com>
2023-10-16 04:23:42 +0000

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Young-Lord/imail-reg-password-recover
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func password_decrypt(username, password string) string {
+	// parse password as hex
+	var password_bytes, username_bytes []byte
+	for i := 0; i < len(password); i += 2 {
+		var tmp string = password[i : i+2]
+		var tmp_int int
+		fmt.Sscanf(tmp, "%x", &tmp_int)
+		password_bytes = append(password_bytes, byte(tmp_int))
+	}
+	username_bytes = []byte(username)
+
+	// decrypt
+	username_bytes = bytes.Repeat(username_bytes, len(password_bytes)/len(username_bytes)+1)
+	var result []byte
+	for i := 0; i < len(password_bytes); i++ {
+		result = append(result, password_bytes[i]-username_bytes[i])
+	}
+	return string(result)
+}
+
+func remove_char_around(s string) string {
+	return strings.Trim(s, "\"")
+}
+
+func parse_config_line(line string) (string, string) {
+	splitn := strings.SplitN(line, "=", 2)
+	if len(splitn) != 2 {
+		return "", ""
+	}
+	key := remove_char_around(splitn[0])
+	value := remove_char_around(splitn[1])
+	return key, value
+}
+
+func main() {
+	for index, filename := range os.Args {
+		if index == 0 {
+			continue
+		}
+
+		// open file
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+		var username, password string
+		accounts := make(map[string]string)
+		var email_domain_name string
+
+		// read file line by line
+		r := bufio.NewReader(file)
+
+		for {
+			line, err := r.ReadString('\n')
+			line = strings.TrimSpace(line)
+			if err != nil && err != io.EOF {
+				panic(err)
+			}
+			if err == io.EOF {
+				break
+			}
+
+			key, value := parse_config_line(line)
+
+			if key == "MailAddr" {
+				username = strings.Split(value, "@")[0]
+				email_domain_name = strings.Split(value, "@")[1]
+			}
+			if key == "Password" {
+				if username == "" {
+					panic("username is empty when reading password.")
+				}
+				password = value
+				accounts[username] = password_decrypt(username, password)
+				username, password = "", ""
+			}
+		}
+		file.Close()
+		// fmt.Print(accounts)
+		json_result, _ := json.MarshalIndent(accounts, "", "    ")
+		result_filename := filename + ".dec.json"
+		os.WriteFile(result_filename, json_result, 0644)
+		fmt.Println("Email domain name: " + email_domain_name)
+		fmt.Println("Dumped " + fmt.Sprint(len(accounts)) + " entries to file: " + result_filename)
+	}
+}
